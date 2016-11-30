eacb3aafc81b41586faa6f962927bd0c1d2192f2
Mauricio Noreña <baphometh@gmail.com>
2016-11-29 20:53:06 -0500

    deleted unnecesary comentary on link.rb

--- app/models/link.rb
+++ app/models/link.rb
@@ -11,10 +11,6 @@
 #  updated_at  :datetime         not null
 #
 
-#TODO:  categories as hashtag maybe implement that?
-#define how a link would look? Add a migration for commentary for each link?
-#that if i ll fetch the categories from the comment.
-
 class Link < ApplicationRecord
   #relations
   belongs_to :user
